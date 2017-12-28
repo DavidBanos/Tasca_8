5474490888201e068ac5359ff5b50570e4e18496
Your Name <you@example.com>
2017-12-28 11:02:43 +0100

    Resultats mostrats en una llista

--- readgooglemaps.js
+++ readgooglemaps.js
@@ -17,16 +17,19 @@ function viewJSON() {
     loadJSON(function(response) {
         /* Aqui l'objecte response representa l'objecte JSON que ens 
            ha retornat el servidor */
+           document.getElementById("results").innerHTML = "<ul>";
            for (rest in response.results) {
 
         document.getElementById("results").innerHTML =
-        document.getElementById("results").innerHTML + 
+        document.getElementById("results").innerHTML + "<li>"+
         response.results[rest].name+", "
-        +response.results[rest].geometry.location.lat+" latitud, "
+        +"Coordenades: "+response.results[rest].geometry.location.lat+" latitud, "
         +response.results[rest].geometry.location.lng+" longitud, "
         +"adreça: "+ response.results[rest].vicinity+", "
-        +response.results[rest].types+" ";
+        +"Tipus d'establiment: "+response.results[rest].types+"</li>";
            }
+           document.getElementById("results").innerHTML =
+           document.getElementById("results").innerHTML + "</ul>"
     });
 }
 
